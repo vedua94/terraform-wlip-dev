236500134aa14688e1716e761bfd1a2da39c0e6d
Ankit Bedua <ankitbedua93@gmail.com>
2024-06-13 14:18:16 +0000

    added wfif

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+    cloud {
+      organization = "ankit_org"
+      workspaces {
+        name = "terraform_dev_env"
+      }
+    }
+  }
+
+
+provider "google" {
+  project = var.gcp_project_id
+  region  = "global"
+  credentials = var.GOOGLE_CREDENTIALS
+}
+
+provider "google-beta" {
+  project = var.gcp_project_id
+  region  = "global"
+  credentials = var.GOOGLE_CREDENTIALS
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+tfc_organization_name = "ankit_org"
+gcp_project_id = "terraform-wlip-dev"
